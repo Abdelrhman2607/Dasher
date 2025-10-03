ef22836184e1d25e57863f8de213610e23f4760c
Abdelrahman <abdelrhman2607@gmail.com>
2025-10-03 17:47:26 -0600

    WIP boss idle animation

--- code/settings.py
+++ code/settings.py
@@ -12,3 +12,14 @@ TILE_SIZE = 128
 PLAYER_HEIGHT = 108
 PLAYER_WIDTH = 49
 
+BOSS_ANIMS = { 
+                "front": [100, True],
+                "blink": [100, False],
+                "fins": [100, False],
+                "jump": [100, False],
+                "land": [100, False],
+                "lick": [100, False],
+                "stunned": [100, True],
+                "wake": [100, False],
+                "weaken": [100, False]
+            }
--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,6 @@
 from settings import *
 from loaders import *
+from timers import Timer
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, x, y, image, ground, collide, groups):
@@ -26,3 +27,55 @@ class Boss(pygame.sprite.Sprite):
         self.frames = frames_loader("images", "boss")
         self.image = self.frames["front"][0]
         self.rect = self.image.get_frect(center = pos)
+
+        self.state = "front"
+        self.previous_state = "front"
+        self.last_frame_state = "front"
+        self.state_changed = False
+
+        self.frame_index = 0
+        self.anim_length = 0
+
+        self.idle_timer = Timer(duration = 5000,
+                                autostart = True,
+                                reusable = True,
+                                end_func = self.check_idle)
+
+    def animate(self, dt, loop = False):
+        
+        if (self.frame_index == 0):
+            self.animating = True
+            self.anim_length = len(self.frames[self.state])
+
+        if (self.frame_index >= self.anim_length):
+            if loop:
+                self.frame_index = 0
+            else:
+                self.animating = False
+
+        if self.animating:
+            self.frame_index = int(self.frame_index + BOSS_ANIMS[self.state][0] * dt)
+            self.frame_index %= self.anim_length
+            self.image = self.frames[self.state][self.frame_index]
+
+
+    def check_idle(self):
+        if self.state == "front":
+            self.state = choice(["stunned"])
+
+    def check_state(self):
+        if self.state != self.last_frame_state:
+            self.state_changed = True
+        else:
+            self.state_changed = False
+
+        if self.state_changed:
+            self.previous_state = self.last_frame_state
+
+    def update(self, dt):
+        self.check_state()
+        self.idle_timer.update()
+        self.animate(dt, BOSS_ANIMS[self.state][1])
+        print(self.state, self.last_frame_state, self.previous_state)
+
+        
