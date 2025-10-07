bbd553aa9c3f45a3f8361f43ca3be124238cfa5a
Abdelrahman <abdelrhman2607@gmail.com>
2025-10-07 11:06:56 -0600

    expanded timer functions

--- code/timers.py
+++ code/timers.py
@@ -15,7 +15,12 @@ class Timer:
 
         self.start_time = 0
         self.end_time = 0
+        self.pause_time = 0
 
+        self.extended = False
+        self.extended_duration = self.duration
+
+        self.paused = False
         self.active = False
         self.in_cooldown = False
 
@@ -23,27 +28,51 @@ class Timer:
             self.start()
 
     def start(self):
-        self.active = True
-        if self.start_func:
-            self.start_func()
-        self.start_time = pygame.time.get_ticks()
+        if not(self.in_cooldown):
+            self.active = True
+            if self.start_func:
+                self.start_func()
+            self.start_time = pygame.time.get_ticks()
 
     def end(self):
-        self.active = False
         if self.end_func:
             self.end_func()
         self.end_time = pygame.time.get_ticks()
 
+        self.reset()
+
         if not(self.reusable) and not(self.repeat):
             del self
 
-    # def extend(self, extension):
-    #     self.start_time -= extension
-    #     self.end_time -= extension
+    def toggle_pause(self):
+        if not self.paused:
+            self.pause_time = pygame.time.get_ticks()
+            self.paused = True
+        else:
+            paused_duration = pygame.time.get_ticks() - self.pause_time
+            self.start_time += paused_duration
+            self.paused = False
+    
+    def extend(self, extension): #can only be used once per timer lifetime (or until reset)
+        if not self.extended:
+            self.extended_duration = self.duration + extension
+            self.extended = True
+
+    def reset(self):
+        self.start_time = 0
+        self.end_time = 0
+        self.pause_time = 0
+
+        self.extended = False
+        self.extended_duration = self.duration
+
+        self.paused = False
+        self.active = False
+        self.in_cooldown = False
 
     def update(self):
-        if self.active:
-            if (pygame.time.get_ticks() - self.start_time >= self.duration) and not(self.in_cooldown):
+        if self.active and not(self.paused):
+            if (pygame.time.get_ticks() - self.start_time >= (self.duration if not(self.extended) else self.extended_duration)) and not(self.in_cooldown):
                 self.end()
                 if self.repeat:
                     self.in_cooldown = True
